29fadae48b832f6257f3714ca5adb260962c2b0b
Sebastián Silva <sebastian.silvanavea@gmail.com>
2024-11-25 16:35:57 -0300

    Mejoras 4

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Reemplaza con el nombre de tu repositorio
+  base: '/pudaweedFinanzas', // Reemplaza con el nombre de tu repositorio
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
